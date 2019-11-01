002817a0157c13db6f8dbd07ec2abdfa3bdbdff2
NuclearManD <dylanbrophy@gmail.com>
2019-11-01 16:37:01 -0700

    Assembler improvements

--- as-emuarch.py
+++ as-emuarch.py
@@ -4,18 +4,19 @@ import sys
 args=sys.argv
 if(len(args)==1):
     args.append(input("input filename:"))
-    sys.argv.append('-po')
-ofn=args[1]
+    args.pop(0)
+ofn=args[0]
 try:
     ofn=ofn[:ofn.index('.')]
 except:
     pass
 
-regs=['rax','rbx','rcx','rdx','rsi','rdi','rsp','rpc',
+
+regs=['rax','rbx','rcx','rdx','si','di','sp','pc',
       'r0', 'r1', 'r2', 'r3', 'r4', 'r5', 'cnt', 'cr0',
       'f0', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7']
 
-file=open(args[1])
+file=open(args[0])
 filedat=file.read()
 file.close()
 
@@ -84,19 +85,27 @@ def can_eval(x):
 def errormsg(ln, string):
     print("Error on line "+str(ln)+": "+string)
 def error_missing_arg(ln, cmd):
-    print("Error on line "+str(ln)+": Opcode "+cmd+" missing argument(s)")
+    errormsg(ln, "Opcode "+cmd+" missing argument(s)")
 def error_too_many_args(ln, cmd):
-    print("Error on line "+str(ln)+": Opcode "+cmd+" has too many arguments")
+    errormsg(ln, "Opcode "+cmd+" has too many arguments")
 names = {}
 adr = 0
 out=[]
 lswrt=0
 location = 0
 def emit(s):
-    global lswrt
-    out.append(s)
-    lswrt=location
+    global lswrt, location
+    lswrt = location
+    if type(s) == int:
+        out.append(s & 255)
+        location += 1
+    else:
+        out.append(s)
+        location += 8
 def wr64(x):
+    if type(x)==str:
+        emit(x)
+        return
     emit(((x>>56)+256)&255)
     emit((x>>48)&255)
     emit((x>>40)&255)
@@ -105,12 +114,12 @@ def wr64(x):
     emit((x>>16)&255)
     emit((x>>8)&255)
     emit(x&255)
-def wr32(x):
+def wr32(x : int):
     emit(((x>>24)+256)&255)
     emit((x>>16)&255)
     emit((x>>8)&255)
     emit(x&255)
-def wr16(x):
+def wr16(x : int):
     emit(((x>>8)+256)&255)
     emit(x&255)
 def wr_size(s, x):
@@ -129,15 +138,13 @@ def parseint(x, allow_ref=True):
         return(to_int(x))
     elif x=='$':
         return(location)
-    elif("-po" in args and allow_ref):
-        return (x)
-    elif(x in names.keys()):
-        return (names[x])
-    else:
-        errormsg("'"+tokens[i]+"' is not a valid number, identifier, or symbol.")
-def evaluate(x):
+    elif allow_ref:
+        if x[0] == '.':
+            x = lslbl + x
+        return x
+def old_evaluate(x):
     if x in regs:
-        errormsg("Internal attempt to interpret register as label")
+        errormsg(linenum, "Internal attempt to interpret register as label")
     if is_int(x):
         wr32(to_int(x))
     elif x=='$':
@@ -146,10 +153,19 @@ def evaluate(x):
         emit(x)
 
 MOV_OPS = ['movq', 'movd', 'movw', 'movb']
-        
+LOAD_OPS = ['lodq', 'lodd', 'lodw', 'lodb']
+
+lslbl = ''
+
 for tokens, linenum in token_gen(filedat):
     cmd = tokens[0]
-    if cmd == '.org':
+    if cmd[-1] == ':':
+        if cmd[0] == '.':
+            cmd = lslbl + cmd
+        else:
+            lslbl = cmd
+        names[cmd[:-1]] = location
+    elif cmd == '.org':
         if len(tokens) < 2 or not is_int(tokens[1]):
             errormsg(linenum, ".org : invalid syntax")
         else:
@@ -160,6 +176,16 @@ for tokens, linenum in token_gen(filedat):
         else:
             qzx=tokens[1]
             names[qzx] = evaluate(tokens[2])
+    elif cmd == 'db':
+        i = 1
+        while i < len(tokens):
+            if tokens[i] == '"':
+                for c in tokens[i + 1]:
+                    emit(ord(c))
+                i += 1
+            else:
+                emit(parseint(tokens[i]))
+            i += 1
     elif cmd in MOV_OPS:
         if len(tokens) < 3:
             error_missing_arg(linenum, cmd)
@@ -204,3 +230,46 @@ for tokens, linenum in token_gen(filedat):
             wr16(parseint(tokens[1]))
     elif cmd == 'halt':
         emit(0xFF)
+    elif cmd in LOAD_OPS:
+        size = LOAD_OPS.index(cmd)
+        if len(tokens) > 1:
+            error_too_many_args(linenum, cmd)
+        else:
+            emit(0b11000000 | (size << 2))
+    else:
+        errormsg(linenum, "unknown opcode '"+cmd+"'")
+if '-eo' in args:
+    # export object file
+    with open(ofn + '.eo', 'wb') as f:
+        f.write(len(names.keys()).to_bytes(4, 'big'))
+        for k, v in names.items():
+            f.write(v.to_bytes(8, 'big'))
+            f.write(k.encode())
+            f.write(b'\x00')
+        f.flush()
+        chunks = []
+        c_chunk = []
+        while len(out) > 0:
+            if type(out[0]) == int:
+                c_chunk.append(out[0])
+            else:
+                chunks.append([bytes(c_chunk), out[0].encode()])
+                c_chunk = []
+            out = out[1:]
+        if len(c_chunk) != 0:
+            chunks.append([bytes(c_chunk), b''])
+        for i in chunks:
+            f.write(len(i[0]).to_bytes(4, 'big'))
+            f.write(i[0])
+            f.write(i[1])
+            f.write(b'\x00')
+else:
+    conv = out
+    out = b''
+    for i in conv:
+        if type(i)==int:
+            out += bytes([i])
+        else:
+            out += names[i].to_bytes(8, 'big')
+    with open(ofn + '.bin', 'wb') as f:
+        f.write(out)
